3698ba6161e8c3a23cc6685d4d94a16c04e8a87b
Jesse Davis <jedavis@interactions.com>
2021-12-29 23:48:04 -0500

    day 11 solved

---
+++ day11/day11problem2/main.go
@@ -0,0 +1,189 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Point struct {
+	row    int
+	col    int
+	energy int
+}
+
+func findAdjacentPoints(p *Point, octopuses [][]Point) []*Point {
+	adjacent_points := make([]*Point, 0)
+
+	rows := len(octopuses)
+	cols := len(octopuses[0])
+
+	// from day 9, second part, but with diagonals
+	// top left
+	if p.row == 0 && p.col == 0 {
+		return append(adjacent_points,
+			&octopuses[0][1],
+			&octopuses[1][1],
+			&octopuses[1][0])
+	}
+	// top right
+	if p.row == 0 && p.col == cols-1 {
+		return append(adjacent_points,
+			&octopuses[0][p.col-1],
+			&octopuses[1][p.col-1],
+			&octopuses[1][p.col])
+	}
+	// bottom left
+	if p.row == rows-1 && p.col == 0 {
+		return append(adjacent_points,
+			&octopuses[p.row-1][0],
+			&octopuses[p.row-1][1],
+			&octopuses[p.row][1])
+	}
+	// bottom right
+	if p.row == rows-1 && p.col == cols-1 {
+		return append(adjacent_points,
+			&octopuses[p.row][p.col-1],
+			&octopuses[p.row-1][p.col-1],
+			&octopuses[p.row-1][p.col])
+	}
+	// top edge
+	if p.row == 0 && p.col > 0 && p.col < cols-1 {
+		return append(adjacent_points,
+			&octopuses[0][p.col-1],
+			&octopuses[1][p.col-1],
+			&octopuses[1][p.col],
+			&octopuses[1][p.col+1],
+			&octopuses[0][p.col+1])
+	}
+	// bottom edge
+	if p.row == rows-1 && p.col > 0 && p.col < cols-1 {
+		return append(adjacent_points,
+			&octopuses[p.row][p.col-1],
+			&octopuses[p.row-1][p.col-1],
+			&octopuses[p.row-1][p.col],
+			&octopuses[p.row-1][p.col+1],
+			&octopuses[p.row][p.col+1])
+	}
+	// left edge
+	if p.col == 0 && p.row > 0 && p.row < rows-1 {
+		return append(adjacent_points,
+			&octopuses[p.row-1][0],
+			&octopuses[p.row-1][1],
+			&octopuses[p.row][1],
+			&octopuses[p.row+1][1],
+			&octopuses[p.row+1][0])
+	}
+	// right edge
+	if p.col == cols-1 && p.row > 0 && p.row < rows-1 {
+		return append(adjacent_points,
+			&octopuses[p.row-1][p.col],
+			&octopuses[p.row-1][p.col-1],
+			&octopuses[p.row][p.col-1],
+			&octopuses[p.row+1][p.col-1],
+			&octopuses[p.row+1][p.col])
+	}
+	// everything else
+	return append(adjacent_points,
+		&octopuses[p.row-1][p.col-1],
+		&octopuses[p.row-1][p.col],
+		&octopuses[p.row-1][p.col+1],
+		&octopuses[p.row][p.col+1],
+		&octopuses[p.row+1][p.col+1],
+		&octopuses[p.row+1][p.col],
+		&octopuses[p.row+1][p.col-1],
+		&octopuses[p.row][p.col-1])
+}
+
+func AllFlashed(octopuses [][]Point) bool {
+	for row := 0; row < len(octopuses); row++ {
+		for col := 0; col < len(octopuses[row]); col++ {
+			if octopuses[row][col].energy != 0 {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func main() {
+	f, err := os.Open("../input.txt")
+	//f, err := os.Open("../part1exampledata.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	octopuses := make([][]Point, 0)
+
+	row := 0
+	for scanner.Scan() {
+		points := make([]Point, 0)
+		for col, c := range strings.Split(scanner.Text(), "") {
+			v, _ := strconv.Atoi(c)
+			points = append(points, Point{row, col, v})
+		}
+		octopuses = append(octopuses, points)
+		row++
+	}
+
+	//steps := 200
+	step := 0
+	flashes := 0
+	flash_queue := make([]*Point, 0)
+
+	for {
+		// check to see if we've all flashed
+		if AllFlashed(octopuses) {
+			fmt.Printf("all octopuses flashed after step %d\n", step)
+			break
+		}
+
+		// first, the energy level of each octopus increases by 1
+		for row := 0; row < len(octopuses); row++ {
+			for col := 0; col < len(octopuses[0]); col++ {
+				pos := &octopuses[row][col]
+				(*pos).energy++
+				// if we're going to flash, add to queue
+				if (*pos).energy > 9 {
+					flash_queue = append(flash_queue, pos)
+				}
+			}
+		}
+
+		// next, flash if energy > 9
+		var to_flash *Point
+		for len(flash_queue) > 0 {
+			to_flash, flash_queue = flash_queue[0], flash_queue[1:]
+			if (*to_flash).energy > 9 {
+				flashes++
+				// reset energy to 0, since we flashed
+				(*to_flash).energy = 0
+				// find adjacent octopuses
+				adjacent_octopuses := findAdjacentPoints(to_flash, octopuses)
+				for _, octo := range adjacent_octopuses {
+					// bump energy of adjacent, unless it's 0
+					// if it's zero, that means it already flashed this round
+					// and we only flash once per step
+					if (*octo).energy > 0 {
+						(*octo).energy++
+					}
+					// if adjacent octopus needs to flash, add it to queue
+					if (*octo).energy > 9 {
+						flash_queue = append(flash_queue, octo)
+					}
+				}
+			}
+
+		}
+		step++
+		fmt.Printf("after %d steps, number of flashes = %d\n", step, flashes)
+	}
+}
